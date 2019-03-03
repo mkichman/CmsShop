70c36554d1b6a426acd0b9d135804eac9f3e926c
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-03-03 22:01:28 +0100

    ckeditor and fileman

--- CmsShop/Models/ViewModels/Pages/SidebarVM.cs
+++ CmsShop/Models/ViewModels/Pages/SidebarVM.cs
@@ -1,4 +1,5 @@
 ﻿using CmsShop.Models.Data;
+using System.Web.Mvc;
 
 namespace CmsShop.Models.ViewModels.Pages
 {
@@ -14,6 +15,7 @@ namespace CmsShop.Models.ViewModels.Pages
             Body = row.Body;
         }
         public int Id { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
     }
 }
