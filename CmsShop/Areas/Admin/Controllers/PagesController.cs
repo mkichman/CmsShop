71898a4deb9d19a4c564b37aa5ce0cc7ba337aa2
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-03-03 21:01:18 +0100

    Sorting drag and drop

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -200,5 +200,28 @@ namespace CmsShop.Areas.Admin.Controllers
             // przekierowanie
             return RedirectToAction("Index");
         }
+
+        // POST: Admin/Pages/ReorderPages
+        [HttpPost]
+        public ActionResult ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+                PageDTO dto;
+
+                //sortowanie stron, zapis na bazie
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+
+            return View();
+        }
     }
 }
