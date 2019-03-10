3fe2c9aed6fd221d8cfda8285582920f17b091b6
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-03-10 20:00:53 +0100

    Categories

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Shop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            //deklaracja listy kategorii do wyświetlenia
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+                return View(categoryVMList);
         }
     }
 }
