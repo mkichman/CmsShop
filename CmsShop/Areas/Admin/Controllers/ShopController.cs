170bd0fa99d85285f37dcb8e94e4c3de997cde8a
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-03-12 18:53:28 +0100

    Sortowanie kategorii drag & drop

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -21,5 +21,77 @@ namespace CmsShop.Areas.Admin.Controllers
 
                 return View(categoryVMList);
         }
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory( string catName)
+        {
+            //Deklaracja id
+            string id;
+
+            using (Db db = new Db())
+            {
+                // sprawdzenie czy kategoria jest unikalna
+                if(db.Categories.Any(x => x.Name == catName))
+                    return "tytulzajety";
+
+                // inicjalizacja dto
+                CategoryDTO dto = new CategoryDTO();
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 1000;
+
+                // zapis do bazy
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                // pobieranie id
+                id = dto.Id.ToString();
+            }
+
+            return id;
+        }
+        //POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public ActionResult ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // inicjalizacja licznika
+                int count = 1;
+
+                // deklaraja DTO 
+                CategoryDTO dto;
+
+                // sortowanie kategorii
+                foreach(var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+                return View();
+            }
+
+        // GET: Admin/Shop/DeleteCategory
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                // pobranie kategorii do usunięcia o podanym id 
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //usunięcie kategorii
+                db.Categories.Remove(dto);
+
+                //zapis bazy
+                db.SaveChanges();
+            }
+
+                return RedirectToAction("Categories");
+        }
     }
 }
