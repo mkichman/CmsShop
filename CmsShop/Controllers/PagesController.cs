89379aa3ed8cd88299f8ad9e58804464919f4be3
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-03-21 21:32:13 +0100

    Pages controller

---
+++ CmsShop/Controllers/PagesController.cs
@@ -0,0 +1,54 @@
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            // ustawienie adresu strony
+            if (page == "")
+                page = "home";
+
+            // deklaracja pageVM i pageDTO
+            PageVM model;
+            PageDTO dto;
+
+            // sprawdzenie czy strona istnieje 
+            using (Db db = new Db())
+            {
+                if(! db.Pages.Any(x => x.Slug.Equals(page)))
+                    return RedirectToAction("Index", new { page = "" });
+
+            }
+
+            // pobranie pageDTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // ustawienie tytułu strony
+            ViewBag.PageTitle = dto.Title;
+
+            // sprawdzenie czy strona ma pasek boczny
+            if (dto.HasSidebar == true)
+                ViewBag.SideBar = "Tak";
+            else
+                ViewBag.SideBar = "Nie";
+
+            // inicjalizacja pageVM
+            model = new PageVM(dto);
+
+            // zwrócenie widoku z modelem
+            return View(model);
+        }
+    }
+}
