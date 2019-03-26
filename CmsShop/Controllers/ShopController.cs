f152e2883e706628abc96021d13bba8f7b1aa4d1
Magda <magdalena.kichman@onet.pl>
2019-03-26 11:32:22 +0100

    Categories partial view

---
+++ CmsShop/Controllers/ShopController.cs
@@ -0,0 +1,37 @@
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages");
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            // deklaracja CategoryVM List
+            List<CategoryVM> categoryVMList;
+
+            // inicjalizacja listy
+            using(Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryVM(x))
+                    .ToList();
+            }
+                                   
+            return PartialView(categoryVMList);
+        }
+    }
+}
