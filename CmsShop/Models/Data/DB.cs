c58bb774a24700be86f5b7b2b959bcb2c62ada77
Magda <magdalena.kichman@onet.pl>
2019-04-04 21:43:54 +0200

    Paypal payment

--- CmsShop/Models/Data/DB.cs
+++ CmsShop/Models/Data/DB.cs
@@ -11,5 +11,7 @@ namespace CmsShop.Models.Data
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
     }
 }
