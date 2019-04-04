591ea64238eafe28d5838597054a45c922943c69
Magda <magdalena.kichman@onet.pl>
2019-04-04 09:41:53 +0200

    Rejestracja i logowanie - formularze

--- CmsShop/Controllers/AccountController.cs
+++ CmsShop/Controllers/AccountController.cs
@@ -7,8 +7,21 @@ namespace CmsShop.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            return Redirect("~/account/login");
+        }
+
+        // GET: /account/login
+        public ActionResult Login()
+        {
+            // sprawdzenie czy użytkownik nie jest już zalogowany
+            string username = User.Identity.Name;
+            if (!string.IsNullOrEmpty(username))
+                return RedirectToAction("user-profile");
+
+            // zwrócenie widoku logowania
             return View();
         }
+
 
         // GET: /account/create-account
         [ActionName("create-account")]
@@ -16,5 +29,8 @@ namespace CmsShop.Controllers
         {
             return View("CreateAccount");
         }
+
+
+        
     }
 }
