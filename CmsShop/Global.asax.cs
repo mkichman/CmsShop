6977b4d794d898b5628d8f382ddc5691714ffc0d
Magda <magdalena.kichman@onet.pl>
2019-04-04 11:32:38 +0200

    Role użytkowników

--- CmsShop/Global.asax.cs
+++ CmsShop/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,35 @@ namespace CmsShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null)
+            {
+                return;
+            }
+
+            // pobranie nazwy użytkownika 
+            string username = Context.User.Identity.Name;
+
+            // deklaracja tablicy z rolami
+            string[] roles = null;
+
+            using(Db db = new Db())
+            {
+                // pobranie danych dla uzytkownika z bazy aby pobrać role
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // tworzenie IPrincipal object
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            // aktualizacja Context.User
+            Context.User = newUserObj;
+
         }
     }
 }
