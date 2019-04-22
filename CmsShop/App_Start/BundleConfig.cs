e0865ee9ff85bae96d58fb5d5baa9fc016d319ef
Magda <magdalena.kichman@onet.pl>
2019-04-22 23:39:51 +0200

    front another part

--- CmsShop/App_Start/BundleConfig.cs
+++ CmsShop/App_Start/BundleConfig.cs
@@ -29,8 +29,9 @@ namespace CmsShop
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                       "~/Content/menu.css"
-                       
+                       "~/Content/menu.css",
+                       "~/Content/RWD.css"
+
                       ));
         }
     }
